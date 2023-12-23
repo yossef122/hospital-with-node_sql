449457cec082cdf4b23730b2d16f6503719ff9ba
youssef helmy <96450513+yossef122@users.noreply.github.com>
2023-12-24 01:02:48 +0200

    Add files via upload

--- utils/queries/roomQueries.js
+++ utils/queries/roomQueries.js
@@ -1,29 +1,33 @@
 
 exports.createRoom = `
     CREATE TABLE IF NOT EXISTS Rooms (
-        room_id INT PRIMARY KEY,
+        room_id INT PRIMARY KEY AUTO_INCREMENT,
         dateTime DATE NOT NULL,
-        room_number INT
+        room_number INT,
+        room_kind_name ENUM('operations', 'icu', 'lap', 'regular') NOT NULL
     )
 `;
 
+// For inserting a room
 exports.insertRoom = (req) => {
   return {
     sql: `INSERT INTO Rooms (
-      room_id, room_number, dateTime
+      room_number, dateTime, room_kind_name
     ) VALUES (?, ?, ?);`,
-    values: [req.body.room_id, req.body.room_number, req.body.dateTime],
+    values: [req.body.room_number, req.body.dateTime, req.body.room_kind_name],
   };
 };
 
+// For updating a room
 exports.updateRoom = (req, res) => {
   const newDateTime = req.body.dateTime;
   const newRoomNumber = req.body.room_number;
+  const newRoomKindName = req.body.room_kind_name;
 
-  if (!newDateTime && !newRoomNumber) {
+  if (!newDateTime && !newRoomNumber && !newRoomKindName) {
     return res.status(400).json({
       error:
-        "At least one parameter (dateTime or room_number) is required for the update.",
+        "At least one parameter (dateTime, room_number, or room_kind_name) is required for the update.",
     });
   }
 
@@ -42,6 +46,14 @@ exports.updateRoom = (req, res) => {
     }
     sql += "`room_number` = ?";
     values.push(newRoomNumber);
+  }
+
+  if (newRoomKindName) {
+    if (newDateTime || newRoomNumber) {
+      sql += ", "; // Add a comma if the previous parameters are present
+    }
+    sql += "`room_kind_name` = ?";
+    values.push(newRoomKindName);
   }
 
   sql += " WHERE `Rooms`.`room_id` = ?;";
